7767ebdc811bf0ee80d5d7a1800d797fc1cca393
yuriybat <yuriybat@yahoo.com>
2021-02-23 16:54:15 +0000

    added module

---
+++ image.tf
@@ -0,0 +1,29 @@
+data "aws_ami" "image" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+# data "aws_ami" "company" {
+#   most_recent = true
+#   owners = ["self"] # Canonical
+# }
+
+
+data "aws_availability_zones" "all" {}
+output "AZ" {
+  value = data.aws_availability_zones.all.names
+}
+
+
